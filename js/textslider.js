325a37dde267c72a057177b7c47a01818d6fa027
Mark Phillips <embeepea@git>
2014-12-15 13:32:06 -0500

    refator js version to make it more like cljs version

--- js/textslider.js
+++ js/textslider.js
@@ -1,30 +1,6 @@
-var TextInput = React.createClass({
-  handleInput: function (e) {
-      var value = this.getDOMNode().value.trim();
-      this.props.onValueChange(value);
-  },
-  render: function() {
-    return (
-        <input type="text" size="1" value={this.props.value} onChange={this.handleInput} />
-    );
-  }
-});
-
-var SliderInput = React.createClass({
-  handleInput: function (e) {
-      var value = this.getDOMNode().value;
-      this.props.onValueChange(value);
-  },
-  render: function() {
-    return (
-        <input type="range" min="0" max="10" step="1" value={this.props.value} onInput={this.handleInput} onChange={this.handleInput} />
-    );
-  }
-});
-
 var TextSlider = React.createClass({
-  handleValueChange: function(value) {
-    this.setState({value: value});
+  handleInput: function (e) {
+      this.setState({value: e.target.value});
   },
   getInitialState: function() {
     return {value: 8};
@@ -32,8 +8,9 @@ var TextSlider = React.createClass({
   render: function() {
     return (
       <div className="textSlider">
-        <TextInput value={this.state.value} onValueChange={this.handleValueChange} />
-        <SliderInput value={this.state.value} onValueChange={this.handleValueChange} />
+        <input type="text" size="1" value={this.state.value} onChange={this.handleInput} />
+        <input type="range" min="0" max="10" step="1" value={this.state.value}
+               onInput={this.handleInput} onChange={this.handleInput} />
       </div>
     );
   }
